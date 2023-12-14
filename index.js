f19756b9346068940ec9e97f1dd2bf8b2b31e33f
arjunshekhawat5 <42422394+arjunshekhawat5@users.noreply.github.com>
2023-12-14 13:00:58 +0000

    Add cors and env port

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 morgan.token('person', (request,response) => {
@@ -7,6 +8,7 @@ morgan.token('person', (request,response) => {
     return JSON.stringify(request.body)
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
 
@@ -114,7 +116,7 @@ const generateID = () => {
     return maxId + 1
 }
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
