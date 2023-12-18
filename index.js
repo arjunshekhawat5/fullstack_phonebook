17d6e9e4506d3bc0c8fb7a166aef8b6b88465ce7
arjunshekhawat5 <42422394+arjunshekhawat5@users.noreply.github.com>
2023-12-18 16:10:19 +0000

    Add logic for solutions of 3.15 to 3.18, testing pending with frontend

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ morgan.token('person', (request, response) => {
 })
 
 app.use(cors())
-app.use(express.json())
 app.use(express.static('dist'))
+app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
 
 
@@ -58,7 +58,7 @@ app.get('/info', (request, response) => {
 
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     const id = request.params.id
     //const person = persons.find(p => p.id === id)
     Person.findById(id)
@@ -68,45 +68,28 @@ app.get('/api/persons/:id', (request, response) => {
             }
             response.json(person)
     })
-    .catch(error => {
-        console.error('Error finding person by ID', error)
-        response.status(400).json({error: 'malformatted id'})
-    })
+    .catch(error => next(error))
 })
 
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(p => p.id !== id)
-    console.log(persons)
-    response.status(204).end()
-
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
-    if (!body.name) {
-        console.log('Error : name not present in the post body!');
+    if (!body.name || !body.number) {
+        console.log('Error : name and number must be present!');
         const error = {
-            error: 'Name must be present!'
+            error: 'name and number must be present!'
         }
         response.status(400).json(error)
     }
-    else if (!body.number) {
-        console.log('Error: Number not present in the post body!');
-        const error = {
-            error: 'Number must be present!'
-        }
-        response.status(400).json(error)
-    }
-   // else if (persons.find(p => p.name === body.name)) {
-   //  console.log('Error: Name must be unique!')
-   //   const error = {
-   //       error: 'This name is already in phonebook, name must be unique!'
-   //  }
-   //  response.status(400).json(error)
-   // }
    else {
         const person = new Person({
             name: body.name,
@@ -117,27 +100,39 @@ app.post('/api/persons', (request, response) => {
                 console.log('Saved person to the database!')
                 response.json(savedPerson)
             })
-            .catch(error => {
-                console.error('Error saving person to database!', error)
-                response.status(500).json({error: 'Error saving person to the database!'})
-            })
+            .catch(error => next(error))
     }
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const person = {
+        name: request.body.name,
+        number: request.body.number
+    }
+    //console.log(request.params.id, request.body)
+    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
 
-const generateID = () => {
-    const maxId = persons.length > 0
-        ? Math.max(...persons.map(p => p.id))
-        : 0
-
-    return maxId + 1
-}
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
-
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError'){
+        return response.status(400).send({error: 'malformatted id'})
+    }
+
+    next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
